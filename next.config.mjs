79e3863843daab09916206f5d25ff246510b6fc4
adammKass <adam.kascak@protonmail.com>
2026-01-17 15:54:29 +0000

    -path fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA)
   distDir: "./dist", // Changes the build output directory
+  assetPrefix: "./", // REQUIRED for GH Pages
+  trailingSlash: true, // REQUIRED for routing
 };
 
 export default nextConfig;
